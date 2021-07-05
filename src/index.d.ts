4842d2be22050e402465a9195190d73ff964b445
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2021-07-05 17:08:53 +0800

    typedef: Add more TS and Flow typedef (#9)
    * feature(typedef): Add more TS type and Flow type
    
    * fix flow type def filename

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,10 @@
-export function startUpdateFlow(appUpdateType: string, clientVersionStalenessDays?: string): Promise<string>;
+export declare var updateFlowDict: {
+  IMMEDIATE: 1;
+  FLEXIBLE: 0;
+};
+export function startUpdateFlow(
+  appUpdateType: string,
+  clientVersionStalenessDays?: string
+): Promise<string>;
+export function onCompleteUpdate(): Promise<string>;
+export function checkUpdateAvailability(): Promise<string>;
